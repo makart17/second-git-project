aa7b4df1f94b19eb617d4dcf8485a94fbb46af8a
makart17 <artemiymakarkin@gmail.com>
2025-04-23 00:10:53 +0500

    Изменил строку в конфликтной ветке

--- SecondMain.java
+++ SecondMain.java
@@ -6,7 +6,8 @@ public class SecondMain {
         System.out.println("Введите два числа:");
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-        System.out.println("Сумма двух чисел равна: " + (a + b));
+        System.out.println("Результат: " + a + b);
+
 
 
 
